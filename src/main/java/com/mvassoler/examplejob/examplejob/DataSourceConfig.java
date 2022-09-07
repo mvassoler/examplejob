4f3ba6b4a8c36d5f7979534a69857b1a6d7413f2
Marcos Vassoler <marcosvassoler@yahoo.com.br>
2022-09-07 16:45:49 -0300

    Commit

--- src/main/java/com/mvassoler/examplejob/examplejob/DataSourceConfig.java
+++ src/main/java/com/mvassoler/examplejob/examplejob/DataSourceConfig.java
@@ -1,20 +1,22 @@
 package com.mvassoler.examplejob.examplejob;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import java.beans.PropertyVetoException;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 public class DataSourceConfig {
 
-    @Autowired
-    Environment env;
+    private final Environment env;
+
+    public DataSourceConfig(Environment env) {
+        this.env = env;
+    }
 
     @Primary
     @Bean
@@ -27,14 +29,6 @@ public class DataSourceConfig {
         dataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
         dataSource.setMaxPoolSize(10);
         dataSource.setMaxIdleTime(5);
-
-
-     /*   DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));*/
-
 
         return dataSource;
 
